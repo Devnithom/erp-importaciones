e8912e69134bb0f4a038b6899fee47892838bd14
Devnizon <thomy.calpal@gmail.com>
2026-08-13 01:15:01 -0500

    feat: desglose de stock por ubicacion en la tarjeta del producto

--- catalogo/views.py
+++ catalogo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Producto
 
 def inventario_visual_view(request):
-    # Traemos todos los productos que estén activos en la base de datos
-    productos = Producto.objects.filter(activo=True)
+    # prefetch_related trae todos los stocks y ubicaciones en una sola consulta ultrarrápida
+    productos = Producto.objects.filter(activo=True).prefetch_related('stock_set__ubicacion')
     
     return render(request, 'inventario_visual.html', {'productos': productos})
